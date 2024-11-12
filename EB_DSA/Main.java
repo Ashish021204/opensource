b88544fdd0b81da9f4b0142bce9e547cf0a1be94
Hemanth <hemanth2004b@gmail.com>
2024-11-12 10:30:30 +0530

    Create Main.java

---
+++ EB_DSA/Main.java
@@ -0,0 +1,15 @@
+import util.java.*;
+import java.lang.*;
+class Main
+  {
+    public static void main(String[]args)
+    {
+      int a=5;
+      int b=10;
+      System.out.println(a+b);
+      System.out.println(a-b);
+      System.out.println(a*b);
+      System.out.println(a/b);
+      System.out.println(a%b);
+    }
+  }
